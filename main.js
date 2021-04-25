6941b86dee28d2e246c9c370977a04e36c1cef6e
kevAs <kevassiobo@gmail.com>
2021-04-25 12:07:32 +0200

    base

---
+++ main.js
@@ -0,0 +1,12 @@
+//1
+// let eleves = ["Alexis", "Andy", "Charles", "Dorian", "Farhad", "Ilyas", "Junior", "Kev", "Loïc", "Mihai", "Oussama", "Tania", "William", "Alain"];
+// for (let i = 0;i < eleves.length;i++) {
+//     console.log( "bonjour"+ " " + eleves[i]  );
+    
+// }
+//2
+// let x = prompt('Entrez un nombre');
+// for (let i = 0; i <= x; i++) {
+//     console.log(i);
+    
+// }
